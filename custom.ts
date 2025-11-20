7e44bdd4da1d77befc7a1af4a8a3c33a94dbb9ea
10-biter <ep.student@hotmail.com>
2025-11-20 21:53:44 +0300

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -19,26 +19,25 @@ namespace Clicker {
      * @param e describe parameter here
      */
     //% block
-    export function create_clicker_sprite(): void {
+    export function clickerModeOn(): void {
         let make = "yes"
-        let mysprite = sprites.create(img`
-            . . . . . . . 6 . . . . . . . .
-            . . . . . . 8 6 6 . . . 6 8 . .
-            . . . e e e 8 8 6 6 . 6 7 8 . .
-            . . e 2 2 2 2 e 8 6 6 7 6 . . .
-            . e 2 2 4 4 2 7 7 7 7 7 8 6 . .
-            . e 2 4 4 2 6 7 7 7 6 7 6 8 8 .
-            e 2 4 5 2 2 6 7 7 6 2 7 7 6 . .
-            e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 .
-            e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 .
-            e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 .
-            e 2 4 2 2 2 2 2 2 2 2 2 e c 6 .
-            e 2 2 2 2 2 2 2 4 e 2 e e c . .
-            e e 2 e 2 2 4 2 2 e e e c . . .
-            e e e e 2 e 2 2 e e e c . . . .
-            e e e 2 e e c e c c c . . . . .
-            . c c c c c c c . . . . . . . .
-        `, SpriteKind.Player)
+    }
+
+
+
+
+
+
+    /**
+ * TODO: describe your function here
+ * @param n describe parameter here, eg: 5
+ * @param s describe parameter here, eg: "Hello"
+ * @param e describe parameter here
+ */
+    //% block
+    export function clickerModeOff(): void {
+        let make = "no"
+
     }
 
     /**
